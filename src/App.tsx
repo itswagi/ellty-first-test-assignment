b0adfb63c57eef1e066077008a4cbf16c5749c01
Wajahat Zia <wzia.bscs16seecs@seecs.edu.pk>
2025-11-17 19:13:20 +0500

    chore: added padding

--- src/App.tsx
+++ src/App.tsx
@@ -6,20 +6,22 @@ import { Seperator } from "./components/Seperator";
 
 function App() {
   return (
-    <Card>
-      <div>
-        <ListItem label="All pages" checked={true} />
-      </div>
-      <Seperator />
-      <div className="w-full">
-        <ListItem label="Page 1" checked={true} />
-        <ListItem label="Page 2" checked={true} />
-        <ListItem label="Page 3" checked={true} />
-        <ListItem label="Page 4" checked={true} />
-      </div>
-      <Seperator className="-mt-[3px]" />
-      <Button />
-    </Card>
+    <div className="p-10">
+      <Card>
+        <div>
+          <ListItem label="All pages" checked={true} />
+        </div>
+        <Seperator />
+        <div className="w-full">
+          <ListItem label="Page 1" checked={true} />
+          <ListItem label="Page 2" checked={true} />
+          <ListItem label="Page 3" checked={true} />
+          <ListItem label="Page 4" checked={true} />
+        </div>
+        <Seperator className="-mt-[3px]" />
+        <Button />
+      </Card>
+    </div>
   );
 }
 
